7633d6c1b1255c7beef89985d3f42dd16c10c016
damian-mac <renzo.damian.go@protonmail.com>
2020-04-24 01:10:57 -0500

    Grafos más poderosos

---
+++ fuerzabruta.py
@@ -0,0 +1,4 @@
+import generar_grafo as gg
+
+if __name__ == '__main__':
+    print(gg.generargrafov2(5,8))
--- generar_grafo.py
+++ generar_grafo.py
@@ -1,20 +1,35 @@
-import random
+from random import *
+
+
 # n vertices
 # m aristas
 
-def generargrafo(n,m):
+def generargrafo(n, m):
     grafo = []
-    for i in range(1,n+1):
+    for i in range(1, n + 1):
         arr = [i]
         grafo.append(arr)
     for i in range(m):
         i = i + 1
-        x = random.randint(0,m+1)
+        x = randint(0, m + 1)
         grafo[x].append(i)
+    return grafo
+
+
+def generargrafov2(n, m):
+    grafo = []
+    edge_count = 0
+    for i in range(n):
+        arr = [i]
+        grafo.append(arr)
+        for ari in range(randint(0,n)):
+            aleatorio = getrandbits(1)
+            if ari != i and aleatorio:
+                grafo[i].append(ari)
+
     return grafo
 
 
 if __name__ == '__main__':
     # Genera un grafo de 8 nodos y 6 aristas
-    print(generargrafo(8,6))
-#dadsdadsaas
+    print(generargrafov2(8, 10))
