11d99df31532dd9e850b8583a3063f1d9533bb5b
damian-mac <renzo.damian.go@protonmail.com>
2020-04-25 16:58:30 -0500

    gg

--- fuerzabruta.py
+++ fuerzabruta.py
@@ -8,7 +8,7 @@ def bruteforce(entrada, salida, grafo):
             entrada = grafo[entrada][c]
 
 
-def dfs(entrada, grafo, salida):
+def bfs(entrada, grafo, salida):
     visitados = []
     parent = [None] * len(grafo)
     camino = [salida]
@@ -34,10 +34,41 @@ def dfs(entrada, grafo, salida):
     return "No se encontro camino"
 
 
+def dfs(entrada, grafo, salida):
+    visitados = []
+    parent = [None] * len(grafo)
+    camino = [salida]
+    i = 1
+    frontier = [entrada]
+    while frontier:
+        next = []
+        for u in frontier:
+            for v in grafo[u]:
+                if v not in visitados:
+                    visitados.append(v)
+                    next.insert(0, v)
+                    parent[v] = u
+                if v == salida:
+                    while True:
+                        salida = parent[salida]
+                        camino.append(salida)
+                        if parent[salida] == salida:
+                            return camino[::-1]
+
+        frontier = next
+        i += 1
+    return "No se encontro camino"
+
 
 if __name__ == '__main__':
-    grafo = gg.generargrafov2(8, 25)
+    #grafo = gg.generargrafov2(5, 15)
+
+    grafo = [[0, 1, 2, 4, 3], [1, 2, 3], [2, 0, 3, 4], [3, 2, 0], [4, 0, 1, 2, 3]]
     print(grafo)
-    # grafo = [[0, 4, 2, 3], [1, 2, 3], [2, 0, 1], [3, 0, 2, 4], [4, 1, 2]]
-    camino = dfs(6, grafo, 7)
-    print(camino)
+
+    print("bfs: " + str(bfs(3, grafo, 4)))
+    print("dfs: " + str(dfs(3, grafo, 4)))
+
+# [[0, 1, 2, 4, 3], [1, 2, 3], [2, 0, 3, 4], [3, 2, 0], [4, 0, 1, 2, 3]]
+# [3, 2, 4] BFS
+# DFS
